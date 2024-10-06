b0ce153b4773a38ba3bec59ac93f2222c965ae89
aapetukhov <andreybsongs@gmail.com>
2024-10-06 20:58:54 +0300

    added deepspeech and beamcer templates

--- src/metrics/cer.py
+++ src/metrics/cer.py
@@ -27,3 +27,14 @@ class ArgmaxCERMetric(BaseMetric):
             pred_text = self.text_encoder.ctc_decode(log_prob_vec[:length])
             cers.append(calc_cer(target_text, pred_text))
         return sum(cers) / len(cers)
+    
+class BeamCERMetric(BaseMetric):
+    def __init__(self, text_encoder, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.text_encoder = text_encoder
+
+    def __call__(
+        self, log_probs: Tensor, log_probs_length: Tensor, text: List[str], **kwargs
+    ):
+        # TODO
+        raise NotImplementedError
--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,5 +1,6 @@
-from src.model.baseline_model import BaselineModel
+from src.model.baseline_model import BaselineModel, DeepSpeech2
 
 __all__ = [
     "BaselineModel",
+    "DeepSpeech2"
 ]
