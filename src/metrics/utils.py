08e9cad9e58f74e7beb690e61f1fed9af4d4f048
aapetukhov <andreybsongs@gmail.com>
2024-10-08 16:32:44 +0300

    add collate, cer, wer, todos in utils

--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -1,13 +1,25 @@
+import editdistance
 # Based on seminar materials
 
 # Don't forget to support cases when target_text == ''
 
 
 def calc_cer(target_text, predicted_text) -> float:
-    # TODO
-    pass
+    if not target_text:
+        if predicted_text:
+            return 1
+        return 0
+
+    return editdistance.eval(target_text, predicted_text) / len(target_text)
 
 
 def calc_wer(target_text, predicted_text) -> float:
-    # TODO
-    pass
+    if not target_text:
+        if predicted_text:
+            return 1
+        return 0
+    
+    target_splitted = target_text.split(' ')
+    pred_splitted = predicted_text.split(' ')
+
+    return editdistance.eval(target_splitted, pred_splitted) / len(target_splitted)
