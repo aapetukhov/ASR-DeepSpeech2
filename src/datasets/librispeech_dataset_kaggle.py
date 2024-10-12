0b67d669a37aa87eecced99a21b9bf15c8be63b9
aapetukhov <andreybsongs@gmail.com>
2024-10-12 03:40:07 +0300

    fixed bugs in kaggle dataset

--- src/datasets/librispeech_dataset_kaggle.py
+++ src/datasets/librispeech_dataset_kaggle.py
@@ -26,10 +26,15 @@ class LibrispeechDatasetKaggle(BaseDataset):
     def __init__(self, part, data_dir=None, *args, **kwargs):
         assert part in URL_LINKS or part == "train_all"
 
+        index_path = Path("/kaggle/working/ASR-DeepSpeech2/data_index")
+        index_path.mkdir(exist_ok=True, parents=True)
+        self.index_dir = index_path
+
         if data_dir is None:
-            data_dir = ROOT_PATH / "data" / "datasets" / "librispeech"
-            data_dir.mkdir(exist_ok=True, parents=True)
+            data_dir = ROOT_PATH
+
         self._data_dir = data_dir
+
         if part == "train_all":
             index = sum(
                 [
@@ -55,7 +60,7 @@ class LibrispeechDatasetKaggle(BaseDataset):
         shutil.rmtree(str(self._data_dir / "LibriSpeech"))
 
     def _get_or_load_index(self, part):
-        index_path = self._data_dir / f"{part}_index.json"
+        index_path = self._index_dir / f"{part}_index.json"
         if index_path.exists():
             with index_path.open() as f:
                 index = json.load(f)
